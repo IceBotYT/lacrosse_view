dfd2a5e92526b2d3293e47e0d8af0f14c4cf0024
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2025-02-03 19:54:16 -0500

    Add user agent to requests

--- src/lacrosse_view/util.py
+++ src/lacrosse_view/util.py
@@ -10,6 +10,7 @@ _LOGGER = logging.getLogger(__name__)
 async def request(
     url: str, method: str, websession: aiohttp.ClientSession | None, **kwargs: Any
 ) -> tuple[aiohttp.ClientResponse, dict[str, Any]]:
+    kwargs.setdefault("headers", {})["User-Agent"] = "okhttp/5.0.0-alpha.11"
     if not websession:
         async with aiohttp.ClientSession() as session:
             async with session.request(method, url, **kwargs) as response:
